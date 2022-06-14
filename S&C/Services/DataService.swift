59e28b864faf22e609e1051be4f67c4243bb14ef
amjadoudeh <amjad.oudeh@soundcloud.com>
2022-06-14 20:56:17 +0200

    Create a DataService

---
+++ S&C/Services/DataService.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+class DataService {
+    static let instance = DataService() // static to make it refering to exact same object in the memory
+    
+    private let categories = [
+        Category(title: "SHIRTS", imageName: "shirts.png"),
+        Category(title: "HOODIES", imageName: "hoodies.png"),
+        Category(title: "HATS", imageName: "hats.png"),
+        Category(title: "DIGITAL", imageName: "digital.png")
+    ]
+    
+    func getCategory() -> [Category] {
+        return categories
+    }
+}
