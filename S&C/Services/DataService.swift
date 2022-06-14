f1989d77419aa3bc807f70ca644faa89bced3aac
amjadoudeh <amjad.oudeh@soundcloud.com>
2022-06-14 22:04:48 +0200

    Duplicate the categries to show more

--- S&C/Services/DataService.swift
+++ S&C/Services/DataService.swift
@@ -7,7 +7,10 @@ class DataService {
         Category(title: "SHIRTS", imageName: "shirts.png"),
         Category(title: "HOODIES", imageName: "hoodies.png"),
         Category(title: "HATS", imageName: "hats.png"),
-        Category(title: "DIGITAL", imageName: "digital.png")
+        Category(title: "DIGITAL", imageName: "digital.png"),
+        Category(title: "SHIRTS", imageName: "shirts.png"),
+        Category(title: "HOODIES", imageName: "hoodies.png"),
+        Category(title: "HATS", imageName: "hats.png")
     ]
     
     func getCategory() -> [Category] {
