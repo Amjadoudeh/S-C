1179f6e877265099a6c73dabfdc4534fe83e38b0
amjadoudeh <amjad.oudeh@soundcloud.com>
2022-06-16 10:24:53 +0200

    Create a new VC and collectionView

---
+++ S&C/Views/ProductCell.swift
@@ -0,0 +1,14 @@
+//
+//  ProductCell.swift
+//  S&C
+//
+//  Created by Amajd Oudeh on 16.06.22.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
