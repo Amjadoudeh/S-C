d91a58ce70d94d79a2416cfe64c4eb0666a7a3ca
amjadoudeh <amjad.oudeh@soundcloud.com>
2022-06-16 18:36:58 +0200

    Add the Data to the DataService

---
+++ S&C/Models/Product.swift
@@ -0,0 +1,19 @@
+//
+//  Product.swift
+//  S&C
+//
+//  Created by Amajd Oudeh on 16.06.22.
+//
+
+import Foundation
+struct Product {
+    private(set) public var title: String
+    private(set) public var price: String
+    private(set) public var imageName: String
+    
+    init(title: String, price: String, imageName: String) {
+        self.title = title
+        self.price = price
+        self.imageName = imageName
+    }
+}
--- S&C/Services/DataService.swift
+++ S&C/Services/DataService.swift
@@ -7,10 +7,32 @@ class DataService {
         Category(title: "SHIRTS", imageName: "shirts.png"),
         Category(title: "HOODIES", imageName: "hoodies.png"),
         Category(title: "HATS", imageName: "hats.png"),
-        Category(title: "DIGITAL", imageName: "digital.png"),
-        Category(title: "SHIRTS", imageName: "shirts.png"),
-        Category(title: "HOODIES", imageName: "hoodies.png"),
-        Category(title: "HATS", imageName: "hats.png")
+        Category(title: "DIGITAL", imageName: "digital.png")
+    ]
+    
+    
+    private let hoodies = [
+        Product(title: "Devslopes Logo Beanie", price: "$ 18", imageName: "hoodie01.png"),
+        Product(title: "Devslopes Logo Gray", price: "$ 22", imageName: "hoodie02.png"),
+        Product(title: "Devslopes Logo Dark-Red", price: "$ 24", imageName: "hoodie03.png"),
+        Product(title: "Devslopes Logo Black", price: "$ 32", imageName: "hoodie04.png"),
+        Product(title: "Devslopes Logo Blackish" , price: "$ 12", imageName: "hoodie05.png")
+    ]
+    
+    private let shirts = [
+        Product(title: "Devslopes Logo Beanie", price: "$ 18", imageName: "shirt01.png"),
+        Product(title: "Devslopes Logo Gray", price: "$ 22", imageName: "shirt02.png"),
+        Product(title: "Devslopes Logo Dark-Red", price: "$ 24", imageName: "shirt03.png"),
+        Product(title: "Devslopes Logo Black", price: "$ 32", imageName: "shirt04.png"),
+        Product(title: "Devslopes Logo Blackish" , price: "$ 12", imageName: "shirt05.png")
+    ]
+    
+    private let hats = [
+        Product(title: "Devslopes Logo Beanie", price: "$ 18", imageName: "hat01.png"),
+        Product(title: "Devslopes Logo Gray", price: "$ 22", imageName: "hat02.png"),
+        Product(title: "Devslopes Logo Dark-Red", price: "$ 24", imageName: "hat03.png"),
+        Product(title: "Devslopes Logo Black", price: "$ 32", imageName: "hat04.png"),
+        Product(title: "Devslopes Logo Blackish" , price: "$ 12", imageName: "hat05.png")
     ]
     
     func getCategory() -> [Category] {
